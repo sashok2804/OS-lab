602c55de1e9d3d06cdd8da347234bbcd5400e1b2
sashok2804 <alex.0111127@gmail.com>
2024-10-08 14:00:45 +0400

    fix

--- lab 6/auth.py
+++ lab 6/auth.py
@@ -10,9 +10,8 @@ auth_formatter = logging.Formatter('%(asctime)s - %(message)s')
 auth_handler.setFormatter(auth_formatter)
 auth_logger.addHandler(auth_handler)
 
-# Базовые данные для аутентификации
 USER_CREDENTIALS = {"User": "user_password"}
-CERTIFICATE_NAME = "admin_cert.pem"  # Имя цифрового сертификата Admin
+CERTIFICATE_NAME = "admin_cert.pem"  
 
 # Аутентификация для User
 def authenticate_user(username, password):
@@ -22,16 +21,16 @@ def authenticate_user(username, password):
     auth_logger.warning(f"Неудачная попытка входа для User '{username}'.")
     return False
 
-# Аутентификация для Admin с проверкой имени сертификата
+# Аутентификация для Admin
 def authenticate_admin(cert_path):
-    cert_file_name = os.path.basename(cert_path)  # Извлекаем имя файла из полного пути
+    cert_file_name = os.path.basename(cert_path) 
     if cert_file_name == CERTIFICATE_NAME:
         auth_logger.info("Admin аутентифицирован с использованием сертификата.")
         return True
     auth_logger.warning("Неудачная попытка аутентификации Admin.")
     return False
 
-# Авторизация пользователя для ограничения функционала
+# Авторизация пользователя
 def is_admin(role):
     return role == "Admin"
 
